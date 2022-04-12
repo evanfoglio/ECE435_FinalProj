9f5e26de6b1137061c14353d4ac7c8e97ba6d195
Evan Foglio <evan.foglio@maine.edu>
2022-04-12 10:37:22 -0400

    imported releant libs
    added error checking for command lin args

--- server.py
+++ server.py
@@ -1,3 +1,31 @@
 #!/usr/bin/python3
 
+import socket
+import select
+import sys
+
+from _thread import *
+
+#Error check command line args
+
+if len(sys.argv) != 3:
+    print("Not enough arguments, Example of a corrct call:\n")
+    print("./server <IP ADDR> <PORT NUM>")
+    sys.exit()
+
+#Command line args satisfied
 print("Starting Server...\n")
+
+IP_ADDR = str(sys.argv[1])
+print("IP Address: " + IP_ADDR)
+
+
+PORT = str(sys.argv[2])
+print("Port: " + PORT)
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server.bind(IP_ADDR, PORT)
+
