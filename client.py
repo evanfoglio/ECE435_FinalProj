83c37a489876baac58306989e831f938a9d96fcc
Evan Foglio <evan.foglio@maine.edu>
2022-04-12 18:10:03 -0400

    Server now waits for 1 connection,
    Client connects, sends a message then disconnects.

--- client.py
+++ client.py
@@ -1,3 +1,33 @@
 #!/usr/bin/python3
 
+import socket
+import select
+import sys
+
+
 print("Starting Client...\n")
+
+if len(sys.argv) != 3:
+    print("Not enough arguments, Example of a correct call:\n")
+    print("./client.py <IP ADDR> <PORT NUM>")
+    sys.exit();i
+
+IP_ADDR = str(sys.argv[1])
+print("IP Address: " + IP_ADDR)
+
+
+PORT = int(sys.argv[2])
+print("Port: " + str(PORT))
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.connect((IP_ADDR, PORT))
+
+message = "Hi"
+
+server.send(message.encode())
+server.close()
+
+
+
+
--- server.py
+++ server.py
@@ -9,8 +9,8 @@ from _thread import *
 #Error check command line args
 
 if len(sys.argv) != 3:
-    print("Not enough arguments, Example of a corrct call:\n")
-    print("./server <IP ADDR> <PORT NUM>")
+    print("Not enough arguments, Example of a correct call:\n")
+    print("./server.py <IP ADDR> <PORT NUM>")
     sys.exit()
 
 #Command line args satisfied
@@ -20,12 +20,38 @@ IP_ADDR = str(sys.argv[1])
 print("IP Address: " + IP_ADDR)
 
 
-PORT = str(sys.argv[2])
-print("Port: " + PORT)
+PORT = int(sys.argv[2])
+print("Port: " + str(PORT))
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-server.bind(IP_ADDR, PORT)
+server.bind((IP_ADDR, PORT))
 
+
+server.listen(100)
+
+conn, addr = server.accept()
+ 
+# prints the address of the user that just connected
+print (addr[0] + " connected")
+
+while True:
+    try:
+        message = conn.recv(2048)
+        if message:
+            print(addr[0] + "said " + message.decode())
+            conn.close()
+            server.close()
+            sys.exit()
+    except:
+        continue
+
+
+
+
+
+
+conn.close()
+server.close()
